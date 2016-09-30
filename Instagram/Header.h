fee0a198d67ef5ecc9b186687350c98744d3a7aa
Taro Sakamoto <Taro@taro-no-macbook-air.local>
2016-09-30 21:31:42 +0900

    lesson 9.2

---
+++ Instagram/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  Instagram
+//
+//  Created by Taro Sakamoto on 9/29/16.
+//  Copyright © 2016 Tarou Sakamoto. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  
+//
+//  Created by Taro Sakamoto on 9/30/16.
+//
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
