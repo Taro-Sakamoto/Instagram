721ce6a632eb565ec1820c52774b910975a3cfcc
Taro Sakamoto <Taro@taro-no-macbook-air.local>
2016-10-04 18:56:03 +0900

    Insta課題まで完成

---
+++ Instagram/NSDateFormatter.swift
@@ -0,0 +1,13 @@
+//
+//  NSDateFormatter.swift
+//  Instagram
+//
+//  Created by Taro Sakamoto on 9/30/16.
+//  Copyright © 2016 Tarou Sakamoto. All rights reserved.
+//
+
+import UIKit
+
+class NSDateFormatter: NSLocale {
+
+}
